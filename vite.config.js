90b9dece901254066976c9d71e0472efa772c34d
sanjaykumarreddymanyam <entrepreneur.sanju@gmail.com>
2026-06-15 11:04:44 +0530

    re build vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const backendUrl = env.BACKEND_URL || 'http://localhost:8000'
